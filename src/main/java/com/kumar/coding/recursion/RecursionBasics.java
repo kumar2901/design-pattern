d8c493b5a8b6effd765300c0d021bf35b22fb2c1
kr68011 <kr68011@gmail.com>
2025-10-29 23:37:37 +0530

    palindrome and reverse a number

--- src/main/java/com/kumar/coding/recursion/RecursionBasics.java
+++ src/main/java/com/kumar/coding/recursion/RecursionBasics.java
@@ -18,9 +18,33 @@ public class RecursionBasics {
         return n % 10 + sumOfDigits(n / 10);
     }
 
-    void main(String[] args) {
+    public int reverseNumber(int num, int sum) {
+        if (num < 1) {
+            return sum;
+        }
+        int r = num % 10;
+        return reverseNumber(num / 10, sum * 10 + r);
 
+    }
+
+    public boolean isPalindrome(String str, int l, int r) {
+        if (l >= r) {
+            return true;
+        }
+        if (str.charAt(l) != str.charAt(r)) {
+            return false;
+        }
+
+        return isPalindrome(str, l + 1, r - 1);
+
+
+    }
+
+    void main(String[] args) {
         System.out.println(factorial(5));
         System.out.println(sumOfDigits(1234));
+        System.out.println(reverseNumber(1024, 0));
+        System.out.println(isPalindrome("madam", 0, 4));
+        System.out.println(isPalindrome("hello", 0, 4));
     }
 }
